6dd9d3bc24babec4c55f7c9a6ae1a93b718c528a
Jonathan Hamel <jonathan.hamel87@gmail.com>
2019-11-13 10:28:40 +0100

    Initialization of the project

---
+++ src/test/java/fr/jha/todoapp/TodoappApplicationTests.java
@@ -0,0 +1,13 @@
+package fr.jha.todoapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TodoappApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
